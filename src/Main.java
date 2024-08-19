7e346708c1afa85796063be20b99deecd1bfc028
Borenkov Maksim <borenkovmv@gmail.com>
2024-08-19 22:07:30 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,77 @@
+import java.util.Arrays;
+
+//public class Main {
+//    public static void main(String[] args ) {
+//
+//        // Переменные
+////      System.out.print("Hello \n\t World!");
+////        System.out.println("Hello World!");
+////        int age1 = 56;
+////
+////        age1 = 7;
+////
+////        System.out.println(age1);
+//
+//       // byte -128 до 127 занимает 1 байт
+//       //  int -2147483648   до 214748647 занимает 4 байта
+//        // short -32648 до 32647 занимает 2 байта
+//        //long  -9 223 372 036 854 808 до 9 223 372 036 854 807 занимает 8 байт
+////        byte age = 127;
+////        int num1= 56;
+////        short num2= 128;
+////
+////        float num3 = 235.8f;     //добовляет числа после точки
+////        double num4 = 2.5555d; //добовляет больше чисел после точки
+////       char num5 = '&';  // любой симовл буква все что угодно;
+////        String user_name = "Maks";
+////
+////        System.out.println(user_name);
+//
+//        //------------------------------------------------------------//
+//
+//
+//
+//    }
+//}
+
+import java.util.Scanner;
+
+//public class Main {
+//    public static void main(String[] args ) {
+//        //nextLin()  --водят только строки
+//        //nextInt()  -- водят только числа
+//      Scanner scan = new Scanner(System.in);
+//        System.out.print("Ведите свое имя: ");
+//      String user_name = scan.nextLine();
+//        System.out.println("Привет, " + user_name);
+//
+//        int num1 = scan.nextInt();
+//        byte num2 = scan.nextByte();
+//        boolean num3 =scan.nextBoolean();
+//    }
+//}
+
+public class Main {
+    public static void main(String[] args ) {
+        //nextLin()  --водят только строки
+        //nextInt()  -- водят только числа
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Ведите свое число1: ");
+
+        float sum1 = scan.nextFloat();
+        System.out.print("Ведите свое число2: ");
+
+        float sum2 = scan.nextFloat();
+
+        float res1 = sum1 + sum2;
+        float res2 = sum1 - sum2;
+        float res3 = sum1 * sum2;
+        float res4 = sum1 / sum2;
+
+        System.out.println("Результат: ");
+        System.out.println(res1 + "\n" + res2 + "\n" + res3 + "\n" + res4 );
+
+    }
+}
+
+//Модуль 5
